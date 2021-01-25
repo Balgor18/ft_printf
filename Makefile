c1e905f9f2ad9116a47b9da5e06e871ed18e7bb2
Florian CATINAUD <fcatinau@e2r9p10.42.fr>
2021-01-25 17:02:30 +0100

    First commit

---
+++ Makefile
@@ -0,0 +1,35 @@
+NAME=libftprintf.a
+
+CC=gcc
+
+CFLAGS=-Wall -Wextra -Werror
+
+RM=rm -f
+
+SRC_LIBFT = ft_split.c ft_strjoin.c ft_bzero.c ft_memmove.c ft_memset.c ft_memcpy.c \
+ft_memccpy.c ft_memchr.c ft_atoi.c ft_calloc.c ft_isprint.c ft_isdigit.c \
+ft_isascii.c ft_isalnum.c ft_isalpha.c ft_memcmp.c ft_strchr.c ft_strlen.c \
+ft_strdup.c ft_strlcat.c ft_strlcpy.c ft_strncmp.c ft_strnstr.c ft_strrchr.c \
+ft_toupper.c ft_tolower.c ft_itoa.c ft_putchar_fd.c ft_putendl_fd.c ft_putnbr_fd.c \
+ft_putstr_fd.c ft_strmapi.c ft_strtrim.c ft_substr.c
+
+OBJ_LIBFT = $(SRC_LIBFT:.c=.o)
+
+$(NAME): $(OBJ_LIBFT)
+				ar rc $(NAME) $(OBJ_LIBFT)
+				ranlib $(NAME)
+
+%.o: %.c
+				$(CC) -I. -o $@ -c $? $(CFLAGS)
+
+all: $(NAME)
+
+clean:
+				$(RM) $(OBJ_LIBFT)
+
+fclean: clean
+				$(RM) $(NAME)
+
+re: fclean all
+
+.PHONY: clean clean
---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/25 10:25:28 by fcatinau          #+#    #+#             */
+/*   Updated: 2021/01/25 15:50:43 by fcatinau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft/libft.h"
+# include "stdarg.h"
+
+int		ft_printf(const char *str, va_list ap);
+
+#endif
