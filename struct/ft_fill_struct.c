b4b31992aaf57dd6f8c3d0c6b9f0f5c34db43dc2
Florian CATINAUD <fcatinau@e2r9p10.42.fr>
2021-01-27 16:57:38 +0100

    file not save

--- struct/ft_fill_struct.c
+++ struct/ft_fill_struct.c
@@ -6,7 +6,7 @@
 /*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 15:42:43 by fcatinau          #+#    #+#             */
-/*   Updated: 2021/01/27 16:56:22 by fcatinau         ###   ########.fr       */
+/*   Updated: 2021/01/27 16:57:15 by fcatinau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 /*
 ** Need to change this function when a change the struct
 */
+
 t_var		ft_fill_struct(char *s, t_var var)
 {
 	while (ft_verif_isdigit(*s))
