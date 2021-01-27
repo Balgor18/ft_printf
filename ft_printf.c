228a1ec14a5e7ed731ee79686327c79031a825f4
Florian CATINAUD <fcatinau@e2r9p10.42.fr>
2021-01-27 15:54:46 +0100

    Add comm onthe main function

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/25 10:24:32 by fcatinau          #+#    #+#             */
-/*   Updated: 2021/01/26 16:16:18 by fcatinau         ###   ########.fr       */
+/*   Updated: 2021/01/27 15:41:43 by fcatinau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,27 @@
 
 void	ft(char *str)
 {
-	while(*str)
+	t_var var;
+
+	while (*str)
 	{
 		if (*str == '%')
-		{
-			ft_verif_flags(*str);
-		}
+			var = ft_verif_pourcent(*str);
 		else
 			ft_putchar(*str);
 		str++;
 	}
 }
 
-int		ft_printf(const char *str, va_list ap)
-{
-	va_list		arg;
-
-	//ft_parser(str, '%');
-	//va_start(arg, str);
-	if
-	//va_arg(arg);
-	//va_end(arg);
-}
+/*
+** int		ft_printf(const char *str, va_list ap)
+** {
+** 	va_list		arg;
+**
+** 	//ft_parser(str, '%');
+** 	//va_start(arg, str);
+** 	if
+** 	//va_arg(arg);
+** 	//va_end(arg);
+** }
+*/
